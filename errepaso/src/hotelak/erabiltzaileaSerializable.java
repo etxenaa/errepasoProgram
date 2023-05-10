5d8316bbf434ad31a66a1632367560b7c4a73644
Aimar <118171668+etxenaa@users.noreply.github.com>
2023-05-10 13:03:44 +0200

    Add files via upload

---
+++ errepaso/src/hotelak/erabiltzaileaSerializable.java
@@ -0,0 +1,37 @@
+package hotelak;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class erabiltzaileaSerializable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		erabiltzailea e1 = new erabiltzailea("11111111A", "Lucas", "Lucas123", 0);
+		erabiltzailea e2 = new erabiltzailea("11111111B", "Mario", "MarioBros", 1);
+		erabiltzailea e3 = new erabiltzailea("11111111C", "Pepe", "PepeCalcetin", 2);
+		
+		try {
+			FileOutputStream fos = new FileOutputStream("users.dat");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			// lo grabo
+			oos.writeObject(e1);
+			oos.writeObject(e2);
+			oos.writeObject(e3);
+			
+
+			// cierro el fichero
+			oos.close();
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
